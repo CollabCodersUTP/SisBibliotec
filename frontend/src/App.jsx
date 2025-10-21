6437429b21074c52bd5c3dd480fa0522746c522b
SoportCiberM <soporciber@gmail.com>
2025-10-21 17:07:39 -0500

    frontend y backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import ResponsiveAppBar from './components/navigation'
+
+function App() {
+
+  return (
+    <>
+    <ResponsiveAppBar/>
+    </>
+  );
+}
+
+export default App;

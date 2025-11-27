bc6a5b680225458942b8ecd86bb1a6c11c3048d3
DevJ25 <alavrezsanti@gmail.com>
2025-11-27 15:28:21 -0500

    actualizacion finalizada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import AppLayout from "./components/AppLayout";
-import { LoginRegister } from "./components/LoginRegister";
-=======
 // src/App.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
@@ -12,18 +6,10 @@ import { LoginRegister } from "./components/LoginRegister";
 import PerfilUsuario from "./components/ui/PerfilUsuario";
 import Catalogo from "./components/Catalogo";
 import MainContent from "./components/MainContent";
->>>>>>> origin/main
 
 function App() {
   return (
     <Routes>
-<<<<<<< HEAD
-      {/* Layout principal con sidebar y contenido */}
-      <Route path="/*" element={<AppLayout />} />
-
-      {/* Ruta independiente para login/registro  indefinidos*/}
-      <Route path="/LoginRegister" element={<LoginRegister />} />
-=======
       {/* 🟣 LoginRegister SIN sidebar (Ruta base que maneja login/registro) */}
       <Route path="/*" element={<LoginRegister />} />
 
@@ -35,13 +21,8 @@ function App() {
         <Route path="/account" element={<PerfilUsuario />} />
         <Route path="/dashboard" element={<MainContent />} />
       </Route>
->>>>>>> origin/main
     </Routes>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> origin/main

8c0680733103b9ba53c590ce611ed3c649b150d9
Chris3012Mega <cristofer6666w6@gmail.com>
2025-12-08 00:36:54 -0500

    Prestamo controler CREAR

--- backend/src/main/java/library/main/Controller/PrestamoController.java
+++ backend/src/main/java/library/main/Controller/PrestamoController.java
@@ -1,7 +1,21 @@
 package library.main.Controller;
+@RestController
+@RequestMapping("/prestamos")
+@RequiredArgsConstructor
 
 public class PrestamoController {
+    private final PrestamoService prestamoService;
 
+    // Crear préstamo
 
+    @PostMapping("/crear")
+    public ResponseEntity<Prestamo> crearPrestamo(
+            @RequestParam Long idUsuario,
+            @RequestParam Long idLibro
+    )
+    {
+        Prestamo prestamo = prestamoService.crearPrestamo(idUsuario, idLibro);
+        return ResponseEntity.ok(prestamo);
+    }
 
 }
